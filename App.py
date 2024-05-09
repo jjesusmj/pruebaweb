9bba961139a5d6081ed4a50e8c99f476bc5b4a9d
jjesusmj <160592546+jjesusmj@users.noreply.github.com>
2024-05-09 17:46:00 +0200

    Update App.py

--- App.py
+++ App.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    resultado = ""
     if request.method == "POST":
         valor_a = request.form.get("valor_a")
         valor_b = request.form.get("valor_b")
-        # Aquí puedes hacer la suma y devolver el resultado
-    return render_template("index.html")
+        resultado = int(valor_a) + int(valor_b)
+    return render_template("index.html", resultado=resultado)
 
 if __name__ == "__main__":
     app.run(debug=True)
